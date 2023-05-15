3ae438fd5096e709e5cb2a929937604418d38a36
liyinda246 <liyinda0000@163.com>
2023-05-15 11:05:39 +0800

    vue init

---
+++ vue/src/api/user.js
@@ -0,0 +1,40 @@
+import request from '@/utils/request'
+
+export function getUserList(query) {
+  return request({
+    url: '/home/userlist',
+    method: 'get',
+    params: query
+  })
+}
+
+export function updateUser(data) {
+  return request({
+    url: '/home/useredit',
+    method: 'put',
+    data
+  })
+}
+
+export function addUser(data) {
+  return request({
+    url: '/home/useradd',
+    method: 'post',
+    data
+  })
+}
+
+export function delUser(data) {
+  return request({
+    url: '/home/userdel',
+    method: 'delete',
+    data
+  })
+}
+
+export function getGoogleCode(data) {
+  return request({
+    url: '/home/userlist',
+    method: 'get'
+  })
+}
